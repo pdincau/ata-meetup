751c328122f04b59405745f49225735daf935973
Paolo D'Incau <paolo.dincau@gmail.com>
2018-02-28 12:15:25 +0100

    only things for step1

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,6 @@
 node {
 
     def mvnHome = tool "Maven"
-    def app
 
     stage("Checkout and Test") {
         checkout scm
@@ -10,28 +9,6 @@ node {
 
     stage("Build") {
         sh "${mvnHome}/bin/mvn package"
-        app = docker.build("pdincau/ata-meetup")
-    }
-
-    stage("Test vs Container") {
-        docker.image('pdincau/ata-meetup').withRun('-p 9999:8080') { c ->
-            sh 'sleep 5'
-            sh 'curl -v --fail 127.0.0.1:9999/ping'
-            sh 'curl -s http://127.0.0.1:9999/hello?name=Ivo | grep -a \"Hello Ivo!\"'
-        }
-    }
-
-    stage("Push Image") {
-        app.push("${env.BUILD_NUMBER}")
-    }
-
-    stage("Deploy") {
-        //sh "kubeclt apply -f deployment.yaml"
-        sh "kubectl set image deployment/ata-meetup-deployment ata-meetup=pdincau/ata-meetup:${env.BUILD_NUMBER}"
-    }
-
-    stage("Rest Assured Test") {
-        build job: 'ata-meetup/rest-assured-test'
     }
 
 }
